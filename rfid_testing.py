2f41d7ef932c0e674d2b2d7237cf611f6ca444c4
David Kohorn <dkohorn@silverfallscapital.com>
2025-06-12 09:42:21 -0700

    Cleaned up calculations and output for easier data collection and more universal tag identification

--- rfid_testing.py
+++ rfid_testing.py
@@ -10,37 +10,30 @@ PORT = 'COM4'
 ser = serial.Serial(PORT, 115200, timeout=0.5)
 
 SCAN_FREQUENCY = 0.5
-NUM_READINGS = 5000
+NUM_READINGS = 25
 
 # These will match the EPC hex values to tags from the kit for easier ID during testing
-epc_name_match = defaultdict(lambda: "Incomplete or error tag", {
-    '00471118b06026a28e0114346c': "medium1",
-    '004717929064269d88010e9392': "medium2",
-    '8068940000501a4a9584a57fde': "large",
-    '801190a50300614bffd47b7d31': "small",
-    '000017570d0155255013e89a23': "card"
-})
+# These were obtained through manual scans and data parsing
+known_tags = {
+    '00471118b06026a28e0114346c': "Medium 1",
+    '004717929064269d88010e9392': "Medium 2",
+    '8068940000501a4a9584a57fde': "Large",
+    '801190a50300614bffd47b7d31': "Small",
+    '000017570d0155255013e89a23': "Card"
+}
 
-medium1_scan_count = 0
-medium2_scan_count = 0
-large_scan_count = 0
-small_scan_count = 0
-card_scan_count = 0
+#dictionary for holding relevant info for each tage, updated with the update_totals function
+#formated like tag_name: [scan_count, rssi_count]
+tag_collection_data = {}
 
-medium1_rssi_total = 0
-medium2_rssi_total = 0
-large_rssi_total = 0
-small_rssi_total = 0
-card_rssi_total = 0
-
-
+unknown_tag_count = 0
 
 
 # Return a list [epc, rssi] that is extracted from a frame 
 def extract_epc_rssi(frame):
     frame = frame.hex()
 
-    frame = frame[-44:] # Ensure that any leading bits are cut off from the frame to fix issue of them sometimes not showing up (basically just standardize frame length)
+    frame = frame[-44:] # Ensure that any leading bits are cut off from the frame to fix issue of them sometimes not showing up (basically just standardize frame length) This will get rid of headers like "BB"
     frame = frame[2:] # Remove "type" field 
     frame = frame[2:] # Remove "command" field
     frame = frame[4:] # Remove "payload length" field MSB and LSB
@@ -50,41 +43,24 @@ def extract_epc_rssi(frame):
     frame = frame[8:] # Remove the PC MSB and LSB fields (as well as RSSI), which define extra flags for the tag (dont know what they might be for though)
     frame = frame[:-2] # Remove checksum
 
-    return [frame, rssi]
+    return [frame, int(rssi, 16)] #convert to decimal from hex
 
 
 def update_totals(tag_name, rssi):
-    global medium1_scan_count, medium2_scan_count, large_scan_count, small_scan_count, card_scan_count
-    global medium1_rssi_total, medium2_rssi_total, large_rssi_total, small_rssi_total, card_rssi_total
-
-    if tag_name == "medium1":
-        medium1_scan_count += 1
-        medium1_rssi_total += int(rssi, 16)
-    elif tag_name == "medium2":
-        medium2_scan_count += 1
-        medium2_rssi_total += int(rssi, 16)
-    elif tag_name == "large":
-        large_scan_count += 1
-        large_rssi_total += int(rssi, 16)
-    elif tag_name == "small":
-        small_scan_count += 1
-        small_rssi_total += int(rssi, 16)
-    elif tag_name == "card":
-        card_scan_count += 1
-        card_rssi_total += int(rssi, 16)
-    else:
-        pass
-    
+    if tag_name in tag_collection_data:
+        tag_collection_data[tag_name] = [tag_collection_data[tag_name][0] + 1, tag_collection_data[tag_name][1] + rssi]
+    else: 
+        tag_collection_data[tag_name] = [1, rssi]
 
 
 
 
 
 
-print(f"Beginning inventory (scan every {SCAN_FREQUENCY} seconds)")
+print(f"Beginning inventory (scan {NUM_READINGS} times every {SCAN_FREQUENCY} seconds)")
 
 try:
-    for i in range(NUM_READINGS + 1):
+    for i in range(NUM_READINGS + 1): #Add one extra because first scan is always an error scan
         ser.write(SINGLE_POLL_BYTES)
         data = ser.read(ser.in_waiting or 1)
 
@@ -98,25 +74,28 @@ try:
                 for frame in frames:
                     if frame:
                         tag_info = extract_epc_rssi(frame)
+
+                        if tag_info[0] in known_tags:
+                            print("EPC:", tag_info[0], "| RSSI:", tag_info[1], "| Name:", known_tags[tag_info[0]])
+                            update_totals(known_tags[tag_info[0]], tag_info[1])
+                        else:
+                            print("Unknown, incomplete, or error tag")
+                            unknown_tag_count += 1
                         
-                        print("EPC:", tag_info[0], "| RSSI:", tag_info[1], "| Name:", epc_name_match[tag_info[0]])
-                        update_totals(epc_name_match[tag_info[0]], tag_info[1])
             else:
                 print("Error data frame (nothing scanned):", data)
 
         time.sleep(SCAN_FREQUENCY)
 
     print("\nTotals:")
-    if (medium1_scan_count > 0):
-        print(f"Medium 1 | Scans {medium1_scan_count}/{NUM_READINGS} | Avg RSSI = {medium1_rssi_total / medium1_scan_count}")
-    if (medium2_scan_count > 0):
-        print(f"Medium 2 | Scans {medium2_scan_count}/{NUM_READINGS} | Avg RSSI = {medium2_rssi_total / medium2_scan_count}")
-    if (large_scan_count > 0):
-        print(f"Large | Scans {large_scan_count}/{NUM_READINGS} | Avg RSSI = {large_rssi_total / large_scan_count}")
-    if (small_scan_count > 0):
-        print(f"Small | Scans {small_scan_count}/{NUM_READINGS} | Avg RSSI = {small_rssi_total / small_scan_count}")
-    if (card_scan_count > 0):
-        print(f"Card | Scans {card_scan_count}/{NUM_READINGS} | Avg RSSI = {card_rssi_total / card_scan_count}")
+    print(f"{'Name':<35} | {'Frequency':<10} | {'Avg RSSI':<15}")
+    print("-" * 60)
+    for tag in tag_collection_data:
+        scans = tag_collection_data[tag][0]
+        avg_rssi = int(tag_collection_data[tag][1]) / scans
+        print(f"{tag:<35} | {f'{scans}/{NUM_READINGS}':<10} | {avg_rssi:<15}")
+    print(f"{'Unknown, incomplete, or error tag':<35} | {f'{unknown_tag_count}/{NUM_READINGS}':<10}")
+    
 
 except KeyboardInterrupt:
     print("\nStopping inventory")
